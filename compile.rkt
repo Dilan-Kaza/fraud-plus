2342b707912f84ff4caa8baeda066c746707f1c1
Dilan-Kaza <dkaza0001@gmail.com>
2025-04-08 17:13:52 -0400

    initial commit

---
+++ compile.rkt
@@ -0,0 +1,209 @@
+#lang racket
+(provide (all-defined-out))
+(require "ast.rkt")
+(require "compile-ops.rkt")
+(require "types.rkt")
+(require a86/ast)
+
+(define rax 'rax)
+(define r9  'r9)  ; scratch
+(define rsp 'rsp) ; stack
+(define r15 'r15) ; stack pad (non-volatile)
+
+;; Expr -> Asm
+(define (compile e)
+  (prog (Global 'entry)
+        (Extern 'peek_byte)
+        (Extern 'read_byte)
+        (Extern 'write_byte)
+        (Extern 'raise_error)
+        (Label 'entry)
+        ;; save callee-saved register
+        (Push r15)
+        (compile-e e '())
+        ;; restore callee-save register
+        (Pop r15)
+        (Ret)
+        ;; Error handler
+        (Label 'err)
+        pad-stack
+        (Call 'raise_error)))
+
+;; type CEnv = (Listof [Maybe Id])
+;; Expr CEnv -> Asm
+(define (compile-e e c)
+  (match e
+    [(Lit d)         (compile-value d)]
+    [(Eof)           (compile-value eof)]
+    [(Var x)         (compile-variable x c)]
+    [(Prim0 p)       (compile-prim0 p)]
+    [(Prim1 p e)     (compile-prim1 p e c)]
+    [(Prim2 p e1 e2) (compile-prim2 p e1 e2 c)]
+    ;; TODO: implement n-ary primitives
+    [(PrimN p es)    (compile-primN p es c)]
+    [(If e1 e2 e3)   (compile-if e1 e2 e3 c)]
+    [(Begin e1 e2)   (compile-begin e1 e2 c)]
+    ;; TODO: this only works for single variable binding,
+    ;; make it work in general
+    [(Let x e1 e2)
+     (compile-let x e1 e2 c)]
+    ;; TODO: implement let*, case, cond
+    [(Let* xs es e)  (compile-let* xs es e c)]
+    [(Case ev cs el) (compile-case ev cs el c)]
+    [(Cond cs el)    (compile-cond cs el c)]))
+
+
+;; Value -> Asm
+(define (compile-value v)
+  (seq (Mov rax (value->bits v))))
+
+;; Id CEnv -> Asm
+(define (compile-variable x c)
+  (let ((i (lookup x c)))
+    (seq (Mov rax (Offset rsp i)))))
+
+;; Op0 -> Asm
+(define (compile-prim0 p)
+  (compile-op0 p))
+
+;; Op1 Expr CEnv -> Asm
+(define (compile-prim1 p e c)
+  (seq (compile-e e c)
+       (compile-op1 p)))
+
+;; Op2 Expr Expr CEnv -> Asm
+(define (compile-prim2 p e1 e2 c)
+  (seq (compile-e e1 c)
+       (Push rax)
+       (compile-e e2 (cons #f c))
+       (compile-op2 p)))
+
+(define (compile-primN p es c)
+  (match p
+    ['+ (seq (Mov rax (value->bits 0))
+             (compile-+N es c))]
+    [_ (seq (Jmp 'err))]))
+
+(define (compile-+N es c)
+  (match es
+    ['() '()]
+    [(list e t ...)
+     (seq (Push rax)
+          (compile-e e (cons #f c))
+          (Pop r9)
+          (Add rax r9)
+          (compile-+N t c))]
+    [_ (seq (Jmp 'err))]))
+
+;; HINT: Another potentially helpful function that
+;; emits code to execute each expression and push
+;; all the values on to the stack, analogous to interp*-env
+
+;; [Listof Expr] CEnv -> Asm
+(define (compile-e* es c)
+  (match es
+    ['() (seq)]
+    [(cons e es)
+     (seq (compile-e e c)
+          (Push rax)
+          (compile-e* es (cons #f c)))]))
+
+;; Expr Expr Expr CEnv -> Asm
+(define (compile-if e1 e2 e3 c)
+  (let ((l1 (gensym 'if))
+        (l2 (gensym 'if)))
+    (seq (compile-e e1 c)
+         (Cmp rax (value->bits #f))
+         (Je l1)
+         (compile-e e2 c)
+         (Jmp l2)
+         (Label l1)
+         (compile-e e3 c)
+         (Label l2))))
+;; Expr Expr CEnv -> Asm
+(define (compile-begin e1 e2 c)
+  (seq (compile-e e1 c)
+       (compile-e e2 c)))
+
+(define (compile-cond cs e c)
+  (match cs
+    [(list (Clause e1 e2) a ...)
+     (let (
+           (l1 (gensym 'cond))
+           (l2 (gensym 'cond)))
+          (seq (compile-e e1 c)
+               (Cmp rax (value->bits #f))
+               (Je l1)
+               (compile-e e2 c)
+               (Jmp l2)
+               (Label l1)
+               (compile-cond a e c)
+               (Label l2)))]
+    ['() (compile-e e c)]))
+
+(define (compile-case e1 cs e2 c)
+  (match cs
+            [(list (Clause lst e) a ...)
+             (let ((l1 (gensym 'case))
+                   (l2 (gensym 'case)))
+                  (append (seq (compile-e e1 c)
+                               (Mov r9 rax))
+                          (compile-contains lst l1 c)
+                          (seq (compile-case e1 a e2 c)
+                               (Jmp l2)
+                               (Label l1)
+                               (compile-e e c)
+                               (Label l2))))]
+            ['() (compile-e e2 c)]))
+  
+(define (compile-contains lst jmppnt c)
+  (match lst
+    [(list e t ...)
+                    (append (seq (compile-value e)
+                                 (Cmp rax r9)
+                                 (Je jmppnt))
+                            (compile-contains t jmppnt c))]
+      ['() '()]))
+
+
+;; Id Expr Expr CEnv -> Asm
+;; NOTE: this is specialized for a single variable binding
+;; You should write another function for the general case
+(define (compile-let1 x e1 e2 c)
+  (seq (compile-e e1 c)
+       (Push rax)
+       (compile-e e2 (cons x c))
+       (Add rsp 8)))
+
+(define (compile-let x e1 e2 c)
+  (let ([c+ (add-in-order x c)] [k (count-8 x)]) (seq (compile-e* e1 c)
+       (compile-e e2 c+)
+       (Add rsp k))))
+
+(define (add-in-order x c)
+  (match x
+    ['() c]
+    [(list h t ...) (add-in-order t (cons h c))]))
+(define (count-8 x)
+  (match x
+    ['() 0]
+    [(list h t ...) (+ 8 (count-8 t))]))
+
+(define (compile-let* xs es e2 c)
+  (match xs
+  ['() (compile-e e2 c)]
+  [(list x t ...)
+   (match es
+    [(list e1 et ...)(seq (compile-e e1 c)
+       (Push rax)
+       (compile-let t et e2 (cons x c))
+       (Add rsp 8))])]))
+
+;; Id CEnv -> Integer
+(define (lookup x cenv)
+  (match cenv
+    ['() (error "undefined variable:" x)]
+    [(cons y rest)
+     (match (eq? x y)
+       [#t 0]
+       [#f (+ 8 (lookup x rest))])]))
---
+++ test/tests.rkt
@@ -0,0 +1,231 @@
+#lang racket
+
+(provide make-test-suite
+         make-io-test-suite
+         run-tests)
+
+(require rackunit
+         rackunit/text-ui)
+
+(define (make-test-suite name run-proc)
+  (test-suite
+   name
+
+   ;; Abscond examples
+   (test-suite
+    "Abscond"
+    (check-equal? (run-proc 7) 7)
+    (check-equal? (run-proc -8) -8))
+
+   ;; Blackmail examples
+   (test-suite
+    "Blackmail"
+    (check-equal? (run-proc '(add1 (add1 7))) 9)
+    (check-equal? (run-proc '(add1 (sub1 7))) 7))
+
+   ;; Con examples
+   (test-suite
+    "Con"
+    (check-equal? (run-proc '(if (zero? 0) 1 2)) 1)
+    (check-equal? (run-proc '(if (zero? 1) 1 2)) 2)
+    (check-equal? (run-proc '(if (zero? -7) 1 2)) 2)
+    (check-equal? (run-proc '(if (zero? 0)
+                                 (if (zero? 1) 1 2)
+                                 7))
+                  2)
+    (check-equal? (run-proc '(if (zero? (if (zero? 0) 1 0))
+                                 (if (zero? 1) 1 2)
+                                 7))
+                  7))
+
+   ;; Dupe examples
+   (test-suite
+    "Dupe"
+    (check-equal? (run-proc #t) #t)
+    (check-equal? (run-proc #f) #f)
+    (check-equal? (run-proc '(if #t 1 2)) 1)
+    (check-equal? (run-proc '(if #f 1 2)) 2)
+    (check-equal? (run-proc '(if 0 1 2)) 1)
+    (check-equal? (run-proc '(if #t 3 4)) 3)
+    (check-equal? (run-proc '(if #f 3 4)) 4)
+    (check-equal? (run-proc '(if 0 3 4)) 3)
+    (check-equal? (run-proc '(zero? 4)) #f)
+    (check-equal? (run-proc '(zero? 0)) #t))
+
+   ;; Dupe+ examples
+   (test-suite
+    "Dupe+"
+    (check-equal? (run-proc '(not #t)) #f)
+    (check-equal? (run-proc '(not #f)) #t)
+    (check-equal? (run-proc '(not 7)) #f)
+    (check-equal? (run-proc '(cond [else #t])) #t)
+    (check-equal? (run-proc '(cond [(not #t) 2] [else 3])) 3)
+    (check-equal? (run-proc '(cond [(if #t #t #f) 2] [else 3])) 2)
+    (check-equal? (run-proc '(cond [(zero? 1) 2] [(if (not (zero? (sub1 2))) #t #f) 4] [else 3])) 4)
+    (check-equal? (run-proc '(cond [#t 1] [else 2])) 1)
+    (check-equal? (run-proc '(cond [1 1] [else 2])) 1)
+
+    (check-equal? (run-proc '(case 2 [else 1])) 1)
+    (check-equal? (run-proc '(case 2 [() 3] [else 1])) 1)
+    (check-equal? (run-proc '(case 2 [(2) 3] [else 1])) 3)
+    (check-equal? (run-proc '(case 4 [(2) 3] [else 1])) 1)
+    (check-equal? (run-proc '(case 2 [(7 2) 3] [else 1])) 3)
+    (check-equal? (run-proc '(case 4 [(7 2) 3] [else 1])) 1)
+    (check-equal? (run-proc '(case 2 [(7 2 #t) 3] [else 1])) 3)
+    (check-equal? (run-proc '(case 4 [(7 2 #t) 3] [else 1])) 1)
+    (check-equal? (run-proc '(case #t [(7 2 #t) 3] [else 1])) 3)
+    (check-equal? (run-proc '(case #f [(7 2 #t) 3] [else 1])) 1))
+
+   ;; Dodger examples
+   (test-suite
+    "Dodger"
+    (check-equal? (run-proc #\a) #\a)
+    (check-equal? (run-proc #\b) #\b)
+    (check-equal? (run-proc '(char? #\a)) #t)
+    (check-equal? (run-proc '(char? #t)) #f)
+    (check-equal? (run-proc '(char? 8)) #f)
+    (check-equal? (run-proc '(char->integer #\a)) (char->integer #\a))
+    (check-equal? (run-proc '(integer->char 955)) #\λ))
+
+   ;; Evildoer examples
+   (test-suite
+    "Evildoer"
+    (check-equal? (run-proc '(void)) (void))
+    (check-equal? (run-proc '(begin 1 2)) 2)
+    (check-equal? (run-proc '(eof-object? (void))) #f))
+
+   ;; Extort examples
+   (test-suite
+    "Extort"
+    (check-equal? (run-proc '(add1 #f)) 'err)
+    (check-equal? (run-proc '(sub1 #f)) 'err)
+    (check-equal? (run-proc '(zero? #f)) 'err)
+    (check-equal? (run-proc '(char->integer #f)) 'err)
+    (check-equal? (run-proc '(integer->char #f)) 'err)
+    (check-equal? (run-proc '(integer->char -1)) 'err)
+    (check-equal? (run-proc '(write-byte #f)) 'err)
+    (check-equal? (run-proc '(write-byte -1)) 'err)
+    (check-equal? (run-proc '(write-byte 256)) 'err)
+    (check-equal? (run-proc '(begin (integer->char 97)
+                                    (integer->char 98)))
+                  #\b))
+
+   ;; Fraud examples
+   (test-suite
+    "Fraud"
+    (check-equal? (run-proc '(let ([x 7]) x)) 7)
+    (check-equal? (run-proc '(let ([x 7]) 2)) 2)
+    (check-equal? (run-proc '(let ([x 7]) (add1 x))) 8)
+    (check-equal? (run-proc '(let ([x (add1 7)]) x)) 8)
+    (check-equal? (run-proc '(let ([x 7]) (let ((y 2)) x))) 7)
+    (check-equal? (run-proc '(let ([x 7]) (let ((x 2)) x))) 2)
+    (check-equal? (run-proc '(let ([x 7]) (let ((x (add1 x))) x))) 8)
+    (check-equal? (run-proc '(let ([x 0])
+                               (if (zero? x) 7 8)))
+                  7)
+    (check-equal? (run-proc '(let ([x 1])
+                               (add1 (if (zero? x) 7 8))))
+                  9)
+    (check-equal? (run-proc '(+ 3 4)) 7)
+    (check-equal? (run-proc '(- 3 4)) -1)
+    (check-equal? (run-proc '(+ (+ 2 1) 4)) 7)
+    (check-equal? (run-proc '(+ (+ 2 1) (+ 2 2))) 7)
+    (check-equal? (run-proc '(let ([x (+ 1 2)])
+                               (let ([z (- 4 x)])
+                                 (+ (+ x x) z))))
+                  7)
+    (check-equal? (run-proc '(= 5 5)) #t)
+    (check-equal? (run-proc '(= 4 5)) #f)
+    (check-equal? (run-proc '(= (add1 4) 5)) #t)
+    (check-equal? (run-proc '(< 5 5)) #f)
+    (check-equal? (run-proc '(< 4 5)) #t)
+    (check-equal? (run-proc '(< (add1 4) 5)) #f))
+
+   ;; Fraud+ examples
+   (test-suite
+    "Fraud+"
+    (check-equal? (run-proc '(integer? 7)) #t)
+    (check-equal? (run-proc '(integer? #f)) #f)
+    (check-equal? (run-proc '(boolean? 7)) #f)
+    (check-equal? (run-proc '(boolean? #f)) #t)
+    (check-equal? (run-proc '(boolean? #t)) #t)
+    (check-equal? (run-proc '(let () 7)) 7)
+    (check-equal? (run-proc '(let ([x 1]) x)) 1)
+    (check-equal? (run-proc '(let* () 7)) 7)
+    (check-equal? (run-proc '(let* ([x 1]) x)) 1)
+    (check-equal? (run-proc '(let ([x 1] [y 2]) (+ x y))) 3)
+    (check-equal? (run-proc '(let ([x 1])
+                               (let* ([x 2] [x x]) x)))
+                  2)
+    (check-equal? (run-proc '(+)) 0)
+    (check-equal? (run-proc '(+ 5)) 5)
+    (check-equal? (run-proc '(+ 1 2 3)) 6))
+
+   ;; Student examples
+   (test-suite
+    "Student"
+    ;; TODO: You may want to add more tests here.
+
+    (check-equal? (run-proc '(- 5)) -5)
+    (check-equal? (run-proc '(if (not (zero? (sub1 2))) #t #f)) #t)
+    (check-equal? (run-proc '(if (if (not (zero? (sub1 2))) #t #f) 1 2)) 1)
+    (check-equal? (run-proc '(+ )) 0)
+    (check-equal? (run-proc '(let ([x (+ 1 2)]) (+ x x)))6)
+    (check-equal? (run-proc '(let ([x (+ 1 2)]) x))3)
+    )))
+
+;; NOTE: [run/io-proc] takes two arguments: a quoted input expression and a
+;; string containing the text that can be read by operations like [read-byte].
+;; The result is a pair (built with [cons]) containing the result and a string
+;; containing the output. This means that a properly formed check looks like:
+;;
+;;   (check-equal? (run/io-proc <program> <input>) (cons <result> <output>))
+;;
+;; For example:
+;;
+;;   (check-equal? (run/io-proc '(add1 (read-byte)) "a") (cons 98 ""))
+(define (make-io-test-suite name run/io-proc)
+  (test-suite
+   name
+
+   ;; Evildoer examples
+   (test-suite
+    "Evildoer"
+    (check-equal? (run/io-proc 7 "") (cons 7 ""))
+    (check-equal? (run/io-proc '(write-byte 97) "") (cons (void) "a"))
+    (check-equal? (run/io-proc '(read-byte) "a") (cons 97 ""))
+    (check-equal? (run/io-proc '(begin (write-byte 97) (read-byte)) "b")
+                  (cons 98 "a"))
+    (check-equal? (run/io-proc '(read-byte) "") (cons eof ""))
+    (check-equal? (run/io-proc '(eof-object? (read-byte)) "") (cons #t ""))
+    (check-equal? (run/io-proc '(eof-object? (read-byte)) "a") (cons #f ""))
+    (check-equal? (run/io-proc '(begin (write-byte 97) (write-byte 98)) "")
+                  (cons (void) "ab"))
+    (check-equal? (run/io-proc '(peek-byte) "ab") (cons 97 ""))
+    (check-equal? (run/io-proc '(begin (peek-byte) (read-byte)) "ab") (cons 97 ""))
+    (check-equal? (run/io-proc '(read-byte) "†") (cons 226 "")))
+
+   ;; Extort examples
+   (test-suite
+    "Extort"
+    (check-equal? (run/io-proc '(write-byte #t) "") (cons 'err "")))
+
+   ;; Fraud examples
+   (test-suite
+    "Fraud"
+    (check-equal? (run/io-proc '(let ([x 97]) (write-byte x)) "") (cons (void) "a"))
+    (check-equal? (run/io-proc '(let ([x 97])
+                                  (begin (write-byte x)
+                                         x))
+                               "")
+                  (cons 97 "a"))
+    (check-equal? (run/io-proc '(let ([x 97]) (begin (read-byte) x)) "b")
+                  (cons 97 ""))
+    (check-equal? (run/io-proc '(let ([x 97]) (begin (peek-byte) x)) "b")
+                  (cons 97 "")))
+
+   ;; Student examples
+   (test-suite
+    "Student"
+    ;; TODO: You may want to add more tests here.
+    )))
